525a667a94d1a4c9d157d69a740006960aab0ce9
HaidarMestrah <10120319@mu.edu.lb>
2023-12-24 00:35:18 +0200

    server : added api routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskContoller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,13 @@ Route::group(["middleware" => "auth:api", "prefix" => "user"], function () {
     Route::post("logout", [AuthController::class, "logout"]);
     Route::post("refresh", [AuthController::class, "refresh"]);
     Route::get("profile", [AuthController::class, "profile"]);
+    Route::get("tasks", [TaskContoller::class, "tasks"]);
+    Route::get("markAsDone/{task_id}", [TaskContoller::class, "markAsDone"]);
 
     Route::group(["prefix" => "employer", "middleware" => "employer.valid"], function () {
-        
+        Route::get("create", [TaskContoller::class, "create"]);
+        Route::get("edit/{task_id}", [TaskContoller::class, "edit"]);
+        Route::get("delete/{task_id}", [TaskContoller::class, "delete"]);
     });
 });
 
