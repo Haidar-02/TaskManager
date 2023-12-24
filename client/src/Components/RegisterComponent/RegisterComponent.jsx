6a8f303c630919f73513274d2762adaf1b1d1df7
HaidarMestrah <10120319@mu.edu.lb>
2023-12-24 04:24:41 +0200

    designed login page

--- client/src/Components/RegisterComponent/RegisterComponent.jsx
+++ client/src/Components/RegisterComponent/RegisterComponent.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const RegisterComponent = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <div></div>
+    <div className="flex flex-col items-center justify-center text-white">
+      <h1 className="text-2xl">Register Now</h1>
+      <div className=" mt-5 flex flex-col items-start justify-center">
+        <label htmlFor="name" className="text-sm">
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          autoComplete="off"
+          id="name"
+          className="w-full px-2 py-1 bg-white outline-none text-gray-800 focus:border-b-4 border-yellow-500 rounded-sm transition-all"
+        />
+        <label htmlFor="email" className="text-sm mt-3">
+          Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          autoComplete="off"
+          id="email"
+          className="w-full px-2 py-1 bg-white outline-none text-gray-800 focus:border-b-4 border-yellow-500 rounded-sm transition-all"
+        />
+        <label htmlFor="password" className="text-sm mt-3">
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          autoComplete="off"
+          id="password"
+          className="w-full px-2 py-1 bg-white outline-none text-gray-800 focus:border-b-4 border-yellow-500 rounded-sm transition-all"
+        />
+        <button className="mt-10 bg-white text-gray-800 p-2 text-sm rounded-md w-full hover:bg-green-600 hover:text-white transition-all">
+          Register
+        </button>
+      </div>
+      <div className="text-sm w-full mt-10 fle">
+        Already have an account?{" "}
+        <span
+          onClick={() => {
+            navigate("/login");
+          }}
+          className="hover:underline cursor-pointer"
+        >
+          Signin
+        </span>
+      </div>
     </div>
   );
 };
--- client/src/Pages/Register/RegisterPage.jsx
+++ client/src/Pages/Register/RegisterPage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import RegisterComponent from "../../Components/RegisterComponent/RegisterComponent.jsx";
+import regPic from "../../Assets/Register/register.svg";
 
 const RegisterPage = () => {
   return (
-    <div>
-      <div></div>
+    <div className="flex items-center justify-start bg-gray-200 w-full min-h-screen h-full">
+      <div className="h-full min-h-screen p-10 bg-gray-800 flex items-center justify-center">
+        <RegisterComponent />
+      </div>
+      <div className="flex flex-col gap-10 items-center justify-center flex-grow min-h-screen h-full">
+        <img src={regPic} alt="register" className="w-1/3" />
+      </div>
     </div>
   );
 };
