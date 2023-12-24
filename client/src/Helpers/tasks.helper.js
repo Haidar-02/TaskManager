9fb10f501aac80c6538aafba39c6793f012da332
HaidarMestrah <10120319@mu.edu.lb>
2023-12-24 06:26:40 +0200

    client: implemented task helpers

--- client/src/Helpers/tasks.helper.js
+++ client/src/Helpers/tasks.helper.js
@@ -11,4 +11,59 @@ async function allTasks() {
   }
 }
 
-export { allTasks };
+async function martTaskDone(task_id) {
+  try {
+    const res = await axios.post(
+      `${baseUrl}user/markAsDone/${task_id}`,
+      undefined,
+      auth()
+    );
+    return res;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deleteTask(task_id) {
+  try {
+    const res = await axios.post(`${baseUrl}user/delete/${task_id}`, auth());
+    return res;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createTask({ title, description, due_date }) {
+  try {
+    const res = await axios.post(
+      `${baseUrl}user/employer/create`,
+      {
+        title,
+        description,
+        due_date,
+      },
+      auth()
+    );
+    return res;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function editTask({ title, description, due_date, task_id }) {
+  try {
+    const res = await axios.post(
+      `${baseUrl}user/employer/edit/${task_id}`,
+      {
+        title,
+        description,
+        due_date,
+      },
+      auth()
+    );
+    return res;
+  } catch (error) {
+    throw error;
+  }
+}
+export { allTasks, martTaskDone, deleteTask, createTask, editTask };
