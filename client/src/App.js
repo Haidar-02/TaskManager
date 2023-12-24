bb62f74c9d5ab44e2bf25080bc9d604bbde88bbe
HaidarMestrah <10120319@mu.edu.lb>
2023-12-24 04:01:08 +0200

    modified: landing page

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
--- client/src/Pages/Landing/Landing.jsx
+++ client/src/Pages/Landing/Landing.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import taskPic from "../../Assets/Landing/tasks.svg";
+import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center justify-start bg-gray-200 w-full min-h-screen h-full">
       <header className="flex justify-between items-center py-3 px-10 w-full bg-gray-800 text-white shadow-2xl">
         <h2 className="text-xl tracking-wider">Task Manager</h2>
         <div className="flex gap-5 items-center justify-end">
-          <button className="border-b-2 border-white hover:border-green-500 hover:shadow-md transition-all px-3 py-1">
+          <button
+            onClick={() => {
+              navigate("/register");
+            }}
+            className="border-b-2 border-white hover:border-green-500 hover:shadow-md transition-all px-3 py-1"
+          >
             Register
           </button>
-          <button className="border-b-2 border-white hover:border-yellow-500 hover:shadow-md transition-all px-3 py-1">
+          <button
+            onClick={() => {
+              navigate("/login");
+            }}
+            className="border-b-2 border-white hover:border-yellow-500 hover:shadow-md transition-all px-3 py-1"
+          >
             Login
           </button>
         </div>
@@ -18,7 +30,15 @@ const Landing = () => {
       <div className=" flex-grow flex items-center justify-center gap-5 flex-wrap">
         <div className="flex items-end gap-3 flex-col justify-center p-5 cursor-default">
           <h1 className="text-3xl tracking-widest">Task Manager</h1>
-          <p className="tracking-wider">Manage your tasks now efficiently</p>
+          <p className="tracking-wide">Manage your tasks now efficiently</p>
+          <button
+            onClick={() => {
+              navigate("/register");
+            }}
+            className="bg-gray-800 w-full mt-5 text-white hover:bg-opacity-70 hover:shadow-xl transition-all px-3 py-1 rounded-md"
+          >
+            Join Now
+          </button>
         </div>
         <div className="flex justify-center items-center">
           <img
