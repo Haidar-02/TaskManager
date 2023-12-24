4e2b52e48d6234514a4746c7b1e64e1910400f6e
HaidarMestrah <10120319@mu.edu.lb>
2023-12-24 04:31:49 +0200

    client : implemented login page

--- client/src/Components/LoginComponent/LoginComponent.jsx
+++ client/src/Components/LoginComponent/LoginComponent.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginComponent = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <div></div>
+    <div className="flex flex-col items-center justify-center text-white">
+      <h1 className="text-2xl">Login To Your Account</h1>
+      <div className=" mt-5 flex flex-col items-start justify-center">
+        <label htmlFor="email" className="text-sm mt-3">
+          Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          autoComplete="off"
+          id="email"
+          className="w-full px-2 py-1 bg-white outline-none text-gray-800 focus:border-b-4 border-green-500 rounded-sm transition-all"
+        />
+        <label htmlFor="password" className="text-sm mt-3">
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          autoComplete="off"
+          id="password"
+          className="w-full px-2 py-1 bg-white outline-none text-gray-800 focus:border-b-4 border-green-500 rounded-sm transition-all"
+        />
+        <button className="mt-10 bg-white text-gray-800 p-2 text-sm rounded-md w-full hover:bg-yellow-600 hover:text-white transition-all">
+          Login
+        </button>
+      </div>
+      <div className="text-sm w-full mt-10 fle">
+        Don't have an account?{" "}
+        <span
+          onClick={() => {
+            navigate("/register");
+          }}
+          className="hover:underline cursor-pointer"
+        >
+          Register now
+        </span>
+      </div>
     </div>
   );
 };
--- client/src/Pages/Login/LoginPage.jsx
+++ client/src/Pages/Login/LoginPage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import loginPic from "../../Assets/Login/login.svg";
+import LoginComponent from "../../Components/LoginComponent/LoginComponent.jsx";
 
 const Login = () => {
   return (
-    <div>
-      <div></div>
+    <div className="flex items-center justify-start bg-gray-200 w-full min-h-screen h-full">
+      <div className="flex flex-col gap-10 items-center justify-center flex-grow min-h-screen h-full">
+        <img src={loginPic} alt="login" className="w-1/3" />
+      </div>
+      <div className="h-full min-h-screen p-10 bg-gray-800 flex items-center justify-center">
+        <LoginComponent />
+      </div>
     </div>
   );
 };
